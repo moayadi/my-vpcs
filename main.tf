4602555b88631f9f676ecfb1f07bbed71a1e5ba4
moayadi <42159604+moayadi@users.noreply.github.com>
2020-07-30 10:30:07 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = "${lookup(var.configuration, var.environment).name}"
   cidr = "${lookup(var.configuration, var.environment).vpc_cidr}"
 
-  azs             = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
+  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
